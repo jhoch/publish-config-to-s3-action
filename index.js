25e1a4594b5c37a1523536c28bd4a9b756a65020
Jason Hoch <jhoch@users.noreply.github.com>
2021-03-16 22:33:05 -0700

    First draft of action

---
+++ index.js
@@ -0,0 +1,87 @@
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
+
+const klawSync = require('klaw-sync');
+const core = require('@actions/core');
+const S3 = require('aws-sdk/clients/s3');
+
+const AWS_ACCESS_KEY = core.getInput('aws_access_key', {
+  required: true
+});
+const SECRET_ACCESS_KEY = core.getInput('aws_secret_access_key', {
+  required: true
+});
+const BUCKET = core.getInput('aws_bucket', {
+  required: true
+});
+const SOURCE_DIR = core.getInput('source_dir', {
+  required: true
+});
+const DESTINATION_DIR = core.getInput('destination_dir', {
+  required: true
+});
+const SECRET_HASH_SALT = core.getInput('secret_hash_salt', {
+  required: true
+});
+const DEFAULT_CONFIG_FILE_EXTENSION = '.md';
+const CONFIG_FILE_EXTENSION = core.getInput('config_file_extension', {
+  required: false
+}) ?? DEFAULT_CONFIG_FILE_EXTENSION;
+const DEFAULT_HASH_LENGTH = 12;
+const HASH_LENGTH = core.getInput('hash_length', {
+  required: false
+}) ?? DEFAULT_HASH_LENGTH;
+const DEFAULT_DESTINATION_FILENAME = 'config';
+const DESTINATION_FILENAME = core.getInput('destination_filename', {
+  required: false
+}) ?? DEFAULT_DESTINATION_FILENAME;
+
+const s3 = new S3({
+  accessKeyId: AWS_ACCESS_KEY,
+  secretAccessKey: SECRET_ACCESS_KEY
+});
+const paths = klawSync(SOURCE_DIR, {
+  nodir: true
+});
+
+function upload(params) {
+  return new Promise(resolve => {
+    s3.upload(params, (err, data) => {
+      if (err) core.error(err);
+      core.info(`uploaded - ${data.Key}`);
+      core.info(`located - ${data.Location}`);
+      resolve(data.Location);
+    });
+  });
+}
+
+function run() {
+  const sourceDir = path.join(process.cwd(), SOURCE_DIR);
+  return Promise.all(
+    paths.map(p => {
+      const fileStream = fs.createReadStream(p.path);
+      const basename = path.basename(p.path, CONFIG_FILE_EXTENSION);
+      const hash = crypto.createHmac('sha256', SECRET_HASH_SALT).update(basename).digest('hex').slice(0, HASH_LENGTH)
+      const bucketPath = path.join(DESTINATION_DIR, `${basename}-${hash}`, `${DESTINATION_FILENAME}.${CONFIG_FILE_EXTENSION}`);
+      const params = {
+        Bucket: BUCKET,
+        ACL: 'public-read',
+        Body: fileStream,
+        Key: bucketPath,
+        ContentType: 'text/plain'
+      };
+      return upload(params);
+    })
+  );
+}
+
+run()
+  .then(locations => {
+    core.info(`config locations - ${locations}`);
+    core.setOutput('config_locations', locations);
+  })
+  .catch(err => {
+    core.error(err);
+    core.setFailed(err.message);
+  });
